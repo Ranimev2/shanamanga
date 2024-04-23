21debb4c9ee7de34d5510005116af8d5210de117
Ranime <156202462+Ranimev2@users.noreply.github.com>
2024-04-23 17:27:29 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
          <img class="w-14 rounded-full shadow-xl" src="~/assets/images/user.png" alt="photo user" />
          <div>
             <h3 class="text-sm text-gray-500">Selamat Siang,</h3>
-            <h4 class="text-2xl font-semibold">Rauliqbal</h4>
+            <h4 class="text-2xl font-semibold">shanachan</h4>
          </div>
       </div>
       <HeropageSearch class="mt-8"/>
